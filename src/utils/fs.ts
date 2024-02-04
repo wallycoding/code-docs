ec8c3634aaa23664991117bc0898268d6c8e7e32
wallycoding <wallysonbreno.dev@gmail.com>
2024-02-04 14:23:04 -0300

    BUG-1: check if it exists and just create the folder

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,10 +1,11 @@
 import { lstatSync, existsSync, mkdirSync } from 'node:fs';
+import { dirname } from 'path';
 
 export const isDirectory = (currentPath: string) =>
   lstatSync(currentPath).isDirectory();
 
 export const mkdirExistsOrCreate = (path: string) => {
   if (existsSync(path)) return true;
-  mkdirSync(path, { recursive: true });
+  mkdirSync(dirname(path), { recursive: true });
   return false;
 };
